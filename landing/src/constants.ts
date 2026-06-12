f3fe466d1c9d5e9f226e9f523a3a95e14193ab28
Lakshay Nagda <lakshya18n@gmail.com>
2026-06-12 15:45:19 +0530

    url update

--- landing/src/constants.ts
+++ landing/src/constants.ts
@@ -8,7 +8,7 @@ export const LINKS = {
   playStore: '#',
   
   // APK direct download URL
-  apkDownload: 'https://expo.dev/accounts/daisyapp2026/projects/daisy-app/builds/52efb374-9f92-42ca-b5e9-e1941978bca9',
+  apkDownload: 'https://expo.dev/accounts/daisyapp2026/projects/daisy-app/builds/d69f735f-e580-459c-a25b-6d61824488d5',
   
   // Cloudinary founder profile image (currently pointing to our pre-generated asset!)
   founderPhoto: 'https://res.cloudinary.com/diylru5iv/image/upload/v1781181073/589901796_17842435368640534_1995779922354039761_n_xypxne.jpg',
