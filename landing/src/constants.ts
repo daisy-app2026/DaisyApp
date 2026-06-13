74f767484897a958289fb4839ca225c0949c581f
Lakshay Nagda <lakshya18n@gmail.com>
2026-06-13 15:22:34 +0530

    latest

--- landing/src/constants.ts
+++ landing/src/constants.ts
@@ -2,26 +2,26 @@
 // Meriem can easily replace these placeholders with live Cloudinary/Store links!
 export const LINKS = {
   // App Store Download URL
-  appStore: '#', 
-  
+  appStore: '#',
+
   // Play Store Download URL
   playStore: '#',
-  
+
   // APK direct download URL
-  apkDownload: 'https://expo.dev/accounts/daisyapp2026/projects/daisy-app/builds/d69f735f-e580-459c-a25b-6d61824488d5',
-  
+  apkDownload: 'https://expo.dev/artifacts/eas/PWHg4wYLHZQf51CVBkrl6XToyC1ZEVIgJKXbAM7bfks.apk',
+
   // Cloudinary founder profile image (currently pointing to our pre-generated asset!)
   founderPhoto: 'https://res.cloudinary.com/diylru5iv/image/upload/v1781181073/589901796_17842435368640534_1995779922354039761_n_xypxne.jpg',
-  
+
   // Cloudinary digital diary screen mockup
   diaryScreenshot: 'https://res.cloudinary.com/diylru5iv/image/upload/v1781181099/never_lose_a_feeling_mp1djc.png',
-  
+
   // Cloudinary Talk to Past chat mockup
   talkToPastScreenshot: 'https://res.cloudinary.com/diylru5iv/image/upload/v1781181099/feel_closure_in_minutes_fgvtwt.png',
-  
+
   // Cloudinary Talk to Crush chat mockup
   talkToCrushScreenshot: 'https://res.cloudinary.com/diylru5iv/image/upload/v1781181098/record_and_release_emotions_kgwsjc.png',
-  
+
   // Cloudinary/YouTube demonstration video
   demoVideo: '',
 };
